1c232651a489c30590b215cf19cb3fb501fed8bf
guojingkang <guojingkang@gridsum.com>
2019-11-05 14:58:05 +0800

    更新：开发工具相关

---
+++ src/dev-tools/eslint-config/index.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  extends: [
+    'eslint-config-airbnb',
+    './eslint-rules.js',
+  ].map(require.resolve),
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  plugins: [
+    'promise',
+    'html',
+  ],
+};
